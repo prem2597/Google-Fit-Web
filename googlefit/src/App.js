efd291c9404175f253b47b72f534fbe8fbf3a1d7
Prem Kumar <prembammidi25@msitprogram.net>
2020-12-30 12:14:56 +0530

    data

--- googlefit/src/App.js
+++ googlefit/src/App.js
@@ -785,10 +785,8 @@ class App extends React.Component {
               }}
             >
               <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="time">
-                <Label value="hourly data" position="insideBottomRight" offset={-22}></Label>
-              </XAxis>
-              <YAxis type="number" label={{ value: 'number of steps', angle: -90, position: 'insideBottomLeft' }} domain={[25, 125]}/>
+              <XAxis dataKey="time" />
+              <YAxis />
               <Tooltip />
               <Legend />
               <Bar dataKey="steps" fill="#8884d8" />
@@ -805,10 +803,8 @@ class App extends React.Component {
               }}
             >
               <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="time">
-                <Label value="hourly data" position="insideBottomRight" offset={-22}></Label>
-              </XAxis>
-              <YAxis type="number" label={{ value: 'total number of calories burned', angle: -90, position: 'insideBottomLeft' }} domain={[25, 125]}/>
+              <XAxis dataKey="time" />
+              <YAxis />
               <Tooltip />
               <Legend />
               <Bar dataKey="calories" fill="#82ca9d" />
@@ -825,10 +821,8 @@ class App extends React.Component {
               }}
             >
               <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="time">
-                <Label value="hourly data" position="insideBottomRight" offset={-22}></Label>
-              </XAxis>
-              <YAxis type="number" label={{ value: 'distance travelled in meters', angle: -90, position: 'insideBottomLeft' }} domain={[25, 125]}/>
+              <XAxis dataKey="time" />
+              <YAxis />
               <Tooltip />
               <Legend />
               <Bar dataKey="distance" fill="#ca8286" />
